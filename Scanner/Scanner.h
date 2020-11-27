c4b7c69278f22bc1bb2114f2892328f03e98ca9e
Carl <carl@primer.io>
2020-11-27 00:42:16 +0000

    Initial project setup

---
+++ Scanner/Scanner.h
@@ -0,0 +1,18 @@
+//
+//  Scanner.h
+//  Scanner
+//
+//  Created by Carl Eriksson on 27/11/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Scanner.
+FOUNDATION_EXPORT double ScannerVersionNumber;
+
+//! Project version string for Scanner.
+FOUNDATION_EXPORT const unsigned char ScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Scanner/PublicHeader.h>
+
+
